74ded01dc112aade998fe1aaaf4b11e0bf830deb
Ron Smeral <ron@smeral.net>
2020-03-12 12:28:32 +0100

    fix whitespace in logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,7 +1,7 @@
 type LogFunction = (message?: any, ...optionalParams: any[]) => void;
 
 const wrap = (namespace: string, logFunction: LogFunction): LogFunction =>
-  (args: any[]) => logFunction(`[${namespace}] `, args);
+  (args: any[]) => logFunction(`[${namespace}]`, args);
 
 export const logger = (ns: string) => ({
   debug: wrap(ns, console.debug),
