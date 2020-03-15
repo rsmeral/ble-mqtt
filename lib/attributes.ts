a8292606876f45abda15aa95af4826c960cdb73d
Ron Smeral <ron@smeral.net>
2020-03-15 14:25:57 +0100

    Re-introduce license to files

--- lib/attributes.ts
+++ lib/attributes.ts
@@ -1,3 +1,14 @@
+/*
+ * This file is part of ble-mqtt, a Bluetooth-MQTT bridge.
+ *
+ * Copyright (C)  2016 Gordon Williams <gw@pur3.co.uk>
+ *                2020 Ron Smeral <ron@smeral.net>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 const serviceNames: Record<string, string> = {
   '1809': 'Temperature',
   '180a': 'Device Information',
--- lib/util.ts
+++ lib/util.ts
@@ -1,5 +1,15 @@
+/*
+ * This file is part of ble-mqtt, a Bluetooth-MQTT bridge.
+ *
+ * Copyright (C)  2016 Gordon Williams <gw@pur3.co.uk>
+ *                2020 Ron Smeral <ron@smeral.net>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export const withoutEmpty = (obj: object) =>
   Object.fromEntries(
     Object.entries(obj).filter(([_key, value]) => Boolean(value))
   );
-
